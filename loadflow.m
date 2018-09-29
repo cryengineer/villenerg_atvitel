80ad06156a51db9a20075354bb607a458f3c469f
Balázs <cryengineer@gmail.com>
2018-09-29 18:46:27 +0200

    Add files via upload

--- loadflow.m
+++ loadflow.m
@@ -10,7 +10,7 @@ S_init_6=150+1i*80;
 S_init_4=-100-1i*100*tan(acos(0.9));
 S_init_7=-50-1i*50*tan(acos(0.96));
 S_init_8=-70-1i*70*tan(acos(0.92));
-S_init=[NaN;S_init_4;S_init_6;S_init_7;S_init_8]*1000000;
+S_init=[NaN;S_init_4;S_init_6;S_init_7;S_init_8]*1000000/3; %fazis ertekekkel kell szamolni, osztunk 3-mal
 
 V=zeros(5,1);
 V(1) = 400000/sqrt(3);
@@ -40,5 +40,10 @@ end
 %current and power of slack bus
 I(1)=Y_red(1,:)*V;
 S=V.*conj(I);
-I_full=[I(1);0;0;I(2);0;I(3);I(4);I(5)]
-U=Z*I_full;
+
+%a teljes halozat feszultseg profilja
+I_full=[I(1);0;0;I(2);0;I(3);I(4);I(5)];
+U=Z*I_full*sqrt(3);
+abs(U);
+radtodeg(angle(U));
+sqrt(3)*U(8)*conj(I_full(8))
